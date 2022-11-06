a3bd2fde29288230fca4639dbb4ac09c69582efd
The Geek on Skates <geekonskates@protonmail.com>
2022-11-05 23:46:47 -0400

    Wrote a little program I want to test on the CRT.
    It prints the columns at the top left, and the on the row below it.
    Works great (apart from a PRINT subroutine I haven't cracked yet) on
    both xterm and cool-retro-term.  I'm still learning the ins and outs
    of 16-bit mathing on 8-bit registers, very very hard stuff for anyone
    who (like me) is not a super algebraniac.  Obviously, my skills are
    much more language-related lol... anyway, the point of crawling these
    mathing mazes is so that I can create games that work at multiple
    screen dimensions; for example, my cool-retro-term is at 40x11, which
    is great for me (for my vision) but probably not for most gamers.  On
    my CRT, I expect it'll be something like 40x15, but most people will
    probably run this at full size, 80x24 (but keep in mind the 40x24 max
    the 4000 uses for RAM reasons). Anyway, the interesting thing here is
    how these size limits affect games.  Like look at Space Invaders: the
    enemy space ships have a lot less distance to travel to hit the bottom
    on a 40x11 screen vs. 40x22.  Same deal with Viper (though that game
    doesn't look like it checks for hitting the border).

--- programs/asm/test1.asm
+++ programs/asm/test1.asm
@@ -1,385 +1,78 @@
-; __        __   __________    ______     ________    ______
-; \ \      / /  |____  ____|  |  __  \   |  ______|  |  __  \
-;  \ \    / /       |  |      | |__|  |  |  |___     | |__|  |
-;   \ \  / /        |  |      |   ___/   |  ____|    |     _/
-;    \ \/ /         |  |      |  |       |  |        |  |\ |
-;     \  /       ___|  |____  |  |       |  |______  |  | \ \
-;      \/       |___________| |__|       |________|  |__|  \_\
-; 
-; ==========================================================================================================================================================
-; 
-; Credits:
-; 	"skilldrick", who wrote the tutorial that got me started
-;	in 6502 Assembly programming, and also the original game
-;	http://skilldrick.github.io/easy6502/
-; 
-;	"vkjagt", whose addition to skilldrick's work helped me
-;	understand the code and port it to the Geek-Rig 4000
-; 	https://gist.github.com/wkjagt/9043907
-; 	
-; 
-; ==========================================================================================================================================================
-; MEMORY MAP
-;
-; ADDRESS	WHAT I'M PUTTING THERE
-; $00-01	screen location of apple, stored as two bytes, where the first byte is the least significant.
-; $02		direction:
-; 				1 => up    (bin 0001)
-; 				2 => right (bin 0010)
-; 				4 => down  (bin 0100)
-;				8 => left  (bin 1000)
-; $03		snake length, in number of bytes, not segments
-; $04-05	screen location of snake head stored as two bytes
-; $06-??	snake body (in byte pairs)
-; ==========================================================================================================================================================
-; OTHER NOTES:
-; 	* Change direction with keys: W A S D
-; 	* Unlike the system this game was originally designed for (a web-based system-in-a-browser), the Geek-Rig has 40x24 characters, from $0200-05BF.
-; 	* After the code for the high score screen, the "BYTE" stuff at the bottom sotres the high scores.  This is what will get overwritten at the end,
-; 		when the 4000's disk drive has a write function
-; 
-; ==========================================================================================================================================================
 	ORG $05CC
 	PROCESSOR 6502
 	include "geekrig4000.asm"
 
-MAIN_MENU:
-	; Draw the title logo - not the most efficient way, I know, but it works.
-	LDA #233
-	STA SCREEN + 46
-	LDA #214
-	STA SCREEN + 47
-	STA SCREEN + 48
-	LDA #215
-	STA SCREEN + 49
-	LDA #223
-	STA SCREEN + 50
-	LDA #214
-	STA SCREEN + 86
-	STA SCREEN + 87
-	STA SCREEN + 88
-	LDA #98
-	STA SCREEN + 89
-	LDA #123
-	STA SCREEN + 90
-	LDA #214
-	STA SCREEN + 126
-	STA SCREEN + 166
-	STA SCREEN + 170
-	STA SCREEN + 206
-	STA SCREEN + 210
-	STA SCREEN + 246
-	STA SCREEN + 250
-	STA SCREEN + 286
-	STA SCREEN + 290
-	STA SCREEN + 326
-	STA SCREEN + 327
-	STA SCREEN + 329
-	STA SCREEN + 330
-	STA SCREEN + 367
-	STA SCREEN + 368
-	STA SCREEN + 369
-	STA SCREEN + 172
-	STA SCREEN + 173
-	STA SCREEN + 174
-	STA SCREEN + 175
-	STA SCREEN + 176
-	STA SCREEN + 214
-	STA SCREEN + 254
-	STA SCREEN + 294
-	STA SCREEN + 334
-	STA SCREEN + 372
-	STA SCREEN + 373
-	STA SCREEN + 374
-	STA SCREEN + 375
-	STA SCREEN + 376
-	STA SCREEN + 178
-	STA SCREEN + 179
-	STA SCREEN + 180
-	STA SCREEN + 181
-	STA SCREEN + 218
-	STA SCREEN + 221
-	STA SCREEN + 258
-	STA SCREEN + 259
-	STA SCREEN + 260
-	STA SCREEN + 261
-	STA SCREEN + 298
-	STA SCREEN + 338
-	STA SCREEN + 378
-	STA SCREEN + 183
-	STA SCREEN + 184
-	STA SCREEN + 185
-	STA SCREEN + 186
-	STA SCREEN + 187
-	STA SCREEN + 223
-	STA SCREEN + 263
-	STA SCREEN + 264
-	STA SCREEN + 265
-	STA SCREEN + 303
-	STA SCREEN + 343
-	STA SCREEN + 383
-	STA SCREEN + 384
-	STA SCREEN + 385
-	STA SCREEN + 386
-	STA SCREEN + 387
-	STA SCREEN + 189
-	STA SCREEN + 190
-	STA SCREEN + 191
-	STA SCREEN + 192
-	STA SCREEN + 193
-	STA SCREEN + 229
-	STA SCREEN + 233
-	STA SCREEN + 269
-	STA SCREEN + 270
-	STA SCREEN + 271
-	STA SCREEN + 272
-	STA SCREEN + 309
-	STA SCREEN + 312
-	STA SCREEN + 313
-	STA SCREEN + 349
-	STA SCREEN + 353
-	STA SCREEN + 389
-	STA SCREEN + 393
-	
-	; Draw the text "press any key to start"
-	LDA #16
-	STA SCREEN + 449
-	LDA #18
-	STA SCREEN + 450
-	LDA #5
-	STA SCREEN + 451
-	LDA #19
-	STA SCREEN + 452
-	STA SCREEN + 453
-	
-	LDA #1
-	STA SCREEN + 455
-	LDA #14
-	STA SCREEN + 456
-	LDA #25
-	STA SCREEN + 457
-	
-	LDA #11
-	STA SCREEN + 459
-	LDA #5
-	STA SCREEN + 460
-	LDA #25
-	STA SCREEN + 461
-	
-	LDA #20
-	STA SCREEN + 463
-	LDA #15
-	STA SCREEN + 464
-	
-	LDA #19
-	STA SCREEN + 466
-	LDA #20
-	STA SCREEN + 467
-	STA SCREEN + 470
-	LDA #1
-	STA SCREEN + 468
-	LDA #18
-	STA SCREEN + 469
+MAIN:
+	LDA COLUMNS
+	JSR PRINT_COLUMNS
+	LDA ROWS
+	JSR PRINT_ROWS
+	JMP MAIN
 
-MAIN_MENU_PROMPT:
-	LDA KEY
-	BEQ MAIN_MENU_PROMPT
-	JMP START_GAME
-	
-CLEAR_SCREEN:
-	LDA #$20
+PRINT_COLUMNS:
+	LDX #0				; A = columns, X = 0
+	STX $00				; Now memory address 0 = 0
+	JSR DIVIDE_BY_10	; Divide A by 10; store the 10s place in $00 and the ones in $01
+	LDA $00				; Load the 10s place
+	ADC #48				; So we have characters starting at 48 ('0') not 0 ('@')
+	STA SCREEN			; First char on screen = 10s place
+	LDA $01				; Load 1s place
+	ADC #48				; Again, add 48 to get the right PETSCII character
+	STA SCREEN + 1		; Now the top left corner shows how many columns our terminal has
+	LDA #<SCREEN + 3	; A = least significant byte (LSB) of SCREEN + 3
+	STA $02				; Memory address #2 = A
+	LDA #>SCREEN + 4	; A = most significant byte (MSB) of SCREEN + 3
+	STA $03				; Memory address #3 = A, so now memory address #2 = pointer to SCREEN + 3
+	LDA #<WORD_COLUMNS	; Same idea - make a pointer to WORD_COLUMNS at memory address #4
+	STA $04				; Address $04 = LSB of WORD_COLUMNS
+	LDA #>WORD_COLUMNS	; Same deal
+	STA $05				; Address $05 = MSB of WORD_COLUMNS
+	LDX $00				; Start X at 0, cuz PRINT uses it on a loop
+	JSR PRINT			; Print the word "COLUMNS" at SCREEN + 3
+	RTS					; At least... that was the idea. :)
+
+; Exactly the same as PRINT_COLUMNS except:
+;	1. A contains the number for rows, not columns
+; 	2. I'm printing it on the second row
+; 	3. And since PRINT isn't working yet, I'm not trying to print the word "rows"
+PRINT_ROWS:
 	LDX #0
-CLEAR_SCREEN_CONTINUE:
-	STA SCREEN,X
-	STA SCREEN + 40,X
-	STA SCREEN + 80,X
-	STA SCREEN + 120,X
-	STA SCREEN + 160,X
-	STA SCREEN + 200,X
-	STA SCREEN + 240,X
-	STA SCREEN + 280,X
-	STA SCREEN + 320,X
-	STA SCREEN + 360,X
-	STA SCREEN + 400,X
-	STA SCREEN + 440,X
-	STA SCREEN + 480,X
-	STA SCREEN + 520,X
-	STA SCREEN + 560,X
-	STA SCREEN + 600,X
-	STA SCREEN + 640,X
-	STA SCREEN + 680,X
-	STA SCREEN + 720,X
-	STA SCREEN + 760,X
-	STA SCREEN + 800,X
-	STA SCREEN + 840,X
-	STA SCREEN + 880,X
-	STA SCREEN + 920,X
+	STX $00
+	JSR DIVIDE_BY_10
+	LDA $00
+	ADC #48
+	STA SCREEN + 40
+	LDA $01
+	ADC #48
+	STA SCREEN + 41
+	LDA #<SCREEN + 3
+	STA $02
+	LDA #>SCREEN + 4
+	RTS
+
+; Divides the A-register by 10, storing the 10s place at $00 and the 1s place at $01
+DIVIDE_BY_10:
+	CLC					; Clear the carry flag
+	CMP #10				; This line and the next basically mean,
+	BCC DONE_DIVIDING	; "if (A < 10) goto DONE_DIVIDING;"
+	SBC #10				; if it gets here, now A -= 10;
+	INC $00				; Memory address 0 ++
+	JMP DIVIDE_BY_10	; And call DIVIDE_BY_10 again
+DONE_DIVIDING:
+	STA $01				; Now A is a number < 10
+	RTS					; So store it in memory address 1 and return
+
+PRINT:
+	LDA ($04,X)
+	BEQ DONE_PRINTING
+	STA ($02,X)
 	INX
-	CPX #41
-	BNE CLEAR_SCREEN_CONTINUE
-	RTS
-	
-START_GAME:
-	JSR CLEAR_SCREEN
-	JSR CREATE_SNAKE
-	JSR CREATE_APPLE
-MAIN_LOOP:
-	JSR READ_KEYS
-	JSR CHECK_COLLISIONS
-	JSR DRAW_APPLE
-	JMP MAIN_LOOP
-
-CREATE_SNAKE:
-	LDA #214	; The reverse X, like a scaly snake
-	STA $03F4	; Center of the screen
-  	LDA #2		; direction = right
-	STA $02
-	LDA #4		; remember the head counts for 2 bytes
-	STA $03		; Again, address $03 = snake length
-	
-	LDA #$F4	; We also need to store the head's location
-	STA $04		; in memory addresses $04-05 for later
-	LDA #$03	; Above was low byte (LSB)
-	STA $05		; And here is the high byte (MSB)
-	
-	; Set up the pointers to the snake's body, two LSBs
-	; "one and two places left of the head respectively"
-	LDA #$F3	; Head is at $F4, so one to the left is $F3
-	STA $06		; Snake body starts here
-	LDA #$F2	; Next one to the left is at $F2
-	STA $07		; And store that at the "end of the line"
-	
-	; There was also this bit in the origina; not sure if I need it, but it would go here...
-;	;the most significant bytes of the head and body of the snake
-;	;are all set to hex $04, which is the third 8x32 strip.
-;	lda #$04
-;	sta $11
-;	sta $13
-;	sta $15
+	JMP PRINT
+DONE_PRINTING:
 	RTS
 
-CREATE_APPLE:
-	LDA RANDOM
-	CMP #$C0
-	BCS CREATE_APPLE
-	STA $00
-CREATE_APPLE_HIGH_BYTE:
-	; Ironically, just like the Geek-Rig, the one on Easy 6502
-	; also needs the high byte to be between 2 and 5, so...
-	LDA RANDOM
-	AND #$03 ;mask out lowest 2 bits
-	CLC
-	ADC #2
-	STA $01
-DRAW_APPLE:
-	LDA #$51		; Circle, closest I got to an apple
-	LDY #0			; Cuz I can't just STA (appleL)
-	STA ($00),Y
-	RTS
+WORD_COLUMNS:
+	BYTE $03, $0F, $0C, $15, $0D, $0E, $13, $00
 
-READ_KEYS:
-	LDA KEY
-	CMP #KEY_W
-	BEQ GOING_UP
-	CMP #KEY_D
-	BEQ GOING_DOWN
-	CMP #KEY_D
-	BEQ GOING_RIGHT
-	CMP #KEY_S
-	BEQ GOING_LEFT
-	RTS
-	
-; NOTE (from the original):
-; setting the zero flag if the result of ANDing the two values
-; is 0. So comparing to 4 (bin 0100) only sets zero flag if
-; current direction is 4 (DOWN). So for an illegal move (current
-; direction is DOWN), the result of an AND would be a non zero value
-; so the zero flag would not be set. For a legal move the bit in the
-; new direction should not be the same as the one set for DOWN,
-; so the zero flag needs to be set
-GOING_UP:
-	LDA #4		; Direction = up
-	BIT $02		; AND with value at address $02 (the current direction)
-	BNE ILLEGAL_MOVE
-	LDA #1		; Ending up here means the move is legal
-	STA $02		; ;Store the new direction at address $02
-	RTS
-
-; The other directions work in exactly the same way
-GOING_DOWN:
-	LDA #1
-	BIT $02
-	BNE ILLEGAL_MOVE
-	LDA #4
-	STA $02
-	RTS
-GOING_RIGHT:
-	LDA #8
-	BIT $02
-	BNE ILLEGAL_MOVE
-	LDA #2
-	STA $02
-	RTS
-GOING_LEFT:
-	LDA #2
-	BIT $02
-	BNE ILLEGAL_MOVE
-	LDA #8
-	STA $02
-	; RTS	; Actually I can cut this out, saving a byte for something else
-ILLEGAL_MOVE:
-	RTS	; just return, so the keypress is ignored
-	
-CHECK_COLLISIONS:
-	JSR CHECK_APPLE_COLLISION
-	JSR CHECK_SNAKE_COLLISION
-	RTS
-
-CHECK_APPLE_COLLISION:
-	;check if the snake collided with the apple by comparing the position of the snake's head and the apple.
-	LDA $00		;load value at address $00 (the least significant
-				;byte of the apple's position) into register A
-	CMP $04		;compare to the value stored at address $04
-				;(the least significant byte of the position of the snake's head)
-	BNE DONE_CHECKING_APPLE_COLLISION
-	LDA $01		;load value of address $01 (the most significant byte
-				;of the apple's position) into register A
-	CMP $11		;compare the value stored at address $11 (the most
-				;significant byte of the position of the snake's head)
-	BNE DONE_CHECKING_APPLE_COLLISION
-
-	;Ending up here means the coordinates of the snake head are equal to that of
-	;the apple: eat apple
-	inc $03		 ;increase the snake length
-	inc $03		 ;twice because we're adding two bytes for one segment
-
-	; Create a new apple
-	JSR CREATE_APPLE
-
-	; Update the player's score
-	INC HIGH_SCORE_PLAYER
-DONE_CHECKING_APPLE_COLLISION:
-	RTS
-	
-CHECK_SNAKE_COLLISION:
-	; LEFT OFF HERE
-	RTS
-
-
-
-
-GAME_OVER:
-	; In the original, this was the end of the program.
-	; For Viper, I'd like this to load a high score screen
-	; But for now...
-	JMP MAIN_MENU
-
-; The first byte is the score, and the 3 after it are the player's initials
-HIGH_SCORE_1:
-	BYTE $00, $00, $00, $00
-HIGH_SCORE_2:
-	BYTE $00, $00, $00, $00
-HIGH_SCORE_3:
-	BYTE $00, $00, $00, $00
-HIGH_SCORE_PLAYER:
-	BYTE $00, $00, $00, $00
+WORD_ROWS:
+	BYTE $12, $0f, $17, $13, $00
